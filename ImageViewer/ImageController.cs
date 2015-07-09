809709b87051bb4128ee0cb181ff581b335242ae
gas.script <gas.script@gmail.com>
2015-07-09 10:37:59 +0900

    コミット

---
+++ ImageViewer/ImageController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.IO;    //FileStream
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;      //OpenFileDialog
+
+
+
+namespace ImageViewer
+{
+    class ImageController
+    {
+        private List<String> fileList;     // ファイル名のリスト
+        private static int currentNum;     //現在選択されているファイルの番号
+        private static int filecount;      //ファイルの数
+
+
+        /// <summary>
+        /// プロパティ
+        /// </summary>
+       
+        // 現在選択されているファイルの番号
+        public int currentImageNum
+        {
+            get { return currentNum; }
+          
+        }
+        // ファイルの数
+        public int fileCount
+        {
+            get { return filecount; }
+        }
+
+        /// <summary>
+        /// コンストラクター
+        /// </summary>
+        /// <param name="filelist">ファイルリストのstring配列</param>
+        public ImageController(string[] filelist)
+        {
+            fileList = new List<string>();
+            // メンバの初期化
+            currentNum = 0;
+            filecount = 0;
+
+            foreach (string filename in filelist)
+            {
+                fileList.Add(filename);
+            }
+            //ファイル数を取得
+            filecount = fileList.Count;
+        }
+
+        public BitmapImage Init() {
+            BitmapImage bi = new BitmapImage();
+            bi = getImage();
+            return bi;
+        }
+
+        /// <summary>
+        /// 取得したファイルリストの次の画像を参照
+        /// </summary>
+        /// <returns></returns>
+        public BitmapImage getNextImage()
+        {
+            BitmapImage bi = new BitmapImage();
+            if (currentNum < filecount - 1)
+            {
+                currentNum++;
+            }
+            else if (currentNum == filecount -1)
+            {
+                currentNum = 0;
+            }
+            bi = getImage();
+            return bi;
+        }
+
+        /// <summary>
+        /// 取得したファイルリストの前の画像を取得
+        /// </summary>
+        /// <returns></returns>
+        public BitmapImage getPrevImage()
+        {
+            BitmapImage bi = new BitmapImage();
+            if (currentNum > 0)
+            {
+                currentNum--;
+            }
+            else if (currentNum == 0)
+            {
+                currentNum = filecount - 1;
+            }
+            bi = getImage();
+            return bi;
+        }
+
+        /// <summary>
+        /// ファイル名から画像ファイルを取得
+        /// </summary>
+        /// <returns></returns>
+        private BitmapImage getImage()
+        {
+            BitmapImage bi = new BitmapImage();
+            FileStream fs;
+
+            try
+            {
+                String filename = fileList[currentImageNum];
+                fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+                bi.BeginInit();
+                bi.CacheOption = BitmapCacheOption.OnLoad;
+                bi.StreamSource = fs;
+                bi.EndInit();
+                fs.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace);
+            }
+            finally
+            {
+                
+            }
+            return bi;
+        }
+
+        public TransformedBitmap rotateImage()
+        {
+            TransformedBitmap tb = new TransformedBitmap();
+            return tb;
+        }
+    }
+}
---
+++ ImageViewer/MainWindow.xaml.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.IO;    //FileStream
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;   //Debug
+using Microsoft.Win32;      //OpenFileDialog
+
+
+namespace ImageViewer
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // ファイルリスト
+        List<string> fileList = new List<string>();
+        // 画像読み込み用クラス
+        ImageController controller;
+        // 画像データ保存用BitmapImage
+        BitmapImage imageHandle = new BitmapImage();
+        //すぐに消す
+        RotateTransform rotate = new RotateTransform();
+
+        /// <summary>
+        /// オリジナルコマンド用RoutedCommand
+        /// </summary>
+        public static readonly RoutedCommand ViewFullScreen
+            = new RoutedCommand("ViewFullScreenCmd", typeof(MainWindow));   // 全画面表示コマンド
+        public static readonly RoutedCommand Zoom 
+            = new RoutedCommand("ZoomCmd", typeof(MainWindow));             // 画像拡大コマンド
+        public static readonly RoutedCommand Reduction
+            = new RoutedCommand("ReductionCmd", typeof(MainWindow));        // 画像縮小コマンド
+        public static readonly RoutedCommand RotateRight 
+            = new RoutedCommand("RotateRightCmd", typeof(MainWindow));      // 右回転
+        public static readonly RoutedCommand RotateLeft 
+            = new RoutedCommand("RotateLeftCmd", typeof(MainWindow));       // 左回転
+        public static readonly RoutedCommand FitWindow 
+            = new RoutedCommand("FitWindowCmd", typeof(MainWindow));        // 画面に合わせ拡大
+        public static readonly RoutedCommand FitWidth 
+            = new RoutedCommand("FitWidthCmd", typeof(MainWindow));         // 横幅に合わせ拡大
+        public static readonly RoutedCommand ShowVersion 
+            = new RoutedCommand("ShowVersionCmd", typeof(MainWindow));      // バージョン情報の表示
+
+        public static readonly RoutedCommand getNextImage
+            = new RoutedCommand("GetNextImageCmd", typeof(MainWindow));     // 次の画像を取得
+        public static readonly RoutedCommand getPrevImage
+            = new RoutedCommand("GetPrevImageCmd", typeof(MainWindow));     // 前の画像を取得
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// ファイルを開くコマンドが発行された際の動作
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OpenFileCmd_Executed(object sender, ExecutedRoutedEventArgs e)
+        {            
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.Filter = Properties.Settings.Default.FileFilter;
+            try
+            {
+                bool? result = openFileDialog.ShowDialog();
+                if (result == true)
+                {
+                    controller = new ImageController(openFileDialog.FileNames);
+                    imageHandle = controller.Init();
+                    pictureview1.Source = imageHandle;      
+                }
+
+                Debug.WriteLine("Open -> CurrentNum:" + controller.currentImageNum);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace);
+            }
+            finally
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// 終了コマンドが発行された際の動作
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AppCloseCmd_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// 切り取りコマンドが発行された際の動作
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CutCmd_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            
+        }
+
+        /// <summary>
+        /// コピーコマンドが発行された際の動作
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CopyCmd_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 貼り付けコマンドが発行された際の動作
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PasteCmd_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 全画面表示コマンドが発行された際の動作
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ViewFullScreenCmd_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 画像拡大コマンドが発行された際の動作
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ZoomCmd_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            
+        }
+
+        /// <summary>
+        /// 画像縮小コマンドが発行された際の動作
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ReductionCmd_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 右回転コマンドが発行された際の動作
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RotateRightCmd_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+            TransformedBitmap tb = new TransformedBitmap();
+            tb.BeginInit();
+            tb.Source = imageHandle;
+            rotate.Angle = rotate.Angle + 90;
+            tb.Transform = rotate;
+            tb.EndInit();
+            pictureview1.Source = tb;
+            
+            Debug.WriteLine(rotate.Angle);
+        }
+
+        /// <summary>
+        /// 左回転コマンドが発行された際の動作
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RotateLeftCmd_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 画面に合わせ拡大するコマンドが発行された際の動作
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FitWindowCmd_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 横幅に合わせ拡大するコマンドが発行された際の動作
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FitWidthCmd_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            imageHandle.DecodePixelWidth = imageHandle.PixelWidth * 4;
+            imageHandle.DecodePixelHeight = imageHandle.PixelHeight * 4;
+            pictureview1.Source = imageHandle;
+            
+        }
+
+        /// <summary>
+        /// バージョン情報表示コマンドが発行された際の動作
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ShowVersionCmd_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            
+        }
+
+        /// <summary>
+        /// 次の画像を表示イベントが発生した際の動作
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void getNextImageCmd_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            imageHandle = controller.getNextImage();
+            pictureview1.Source = imageHandle;
+            Debug.WriteLine("CurrentNum:" + controller.currentImageNum);
+        }
+
+        /// <summary>
+        /// 前の画像を表示イベントが発生した際の動作
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void getPrevImageCmd_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            imageHandle = controller.getPrevImage();
+            pictureview1.Source = imageHandle;
+            Debug.WriteLine("CurrentNum:" + controller.currentImageNum);
+        }
+    }
+}
